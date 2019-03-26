02ca3a6cd52d9151713d647bfce6cb749a2261d2
NIIBE Yutaka <gniibe@fsij.org>
2019-03-26 18:18:39 +0900

    Fix typo in copyright notice.

--- contrib/adc-mkl27z.c
+++ contrib/adc-mkl27z.c
@@ -25,7 +25,7 @@
  * As additional permission under GNU GPL version 3 section 7, you may
  * distribute non-source form of the Program without the copy of the
  * GNU GPL normally required by section 4, provided you inform the
- * receipents of GNU GPL by a written offer.
+ * recipients of GNU GPL by a written offer.
  *
  */
 
