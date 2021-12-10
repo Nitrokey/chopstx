82b200a9dd4bead62f6fa1790500ef123cec2d5a
lennard.boediger <lennard@nitrokey.com>
2021-12-10 18:28:00 +0100

    use macros - builds ok 5

--- board/board-nitrokey-start-g.h
+++ board/board-nitrokey-start-g.h
@@ -9,9 +9,9 @@
 // STM32_USBPRE_DIV1P5: GD32 -> STM32_USBPRE_DIV2 STM32 -> STM32_USBPRE_DIV1P5
 #define STM32_USBPRE            (detect_chip()->clock.i_STM32_USBPRE)
 // STM32_PLLMUL_VALUE: GD32 -> 8 STM32 -> 6
-#define STM32_PLLMUL_VALUE      8
+#define STM32_PLLMUL_VALUE      (detect_chip()->clock.i_STM32_PLLMUL_VALUE)
 // STM32_ADCPRE: GD32 -> STM32_ADCPRE_DIV8 STM32 -> STM32_ADCPRE_DIV6 ------ unnecessary?
-#define STM32_ADCPRE            STM32_ADCPRE_DIV8
+#define STM32_ADCPRE            (detect_chip()->clock.i_STM32_ADCPRE)
 
 #define MCU_STM32F1 1
 #define STM32F10X_MD		/* Medium-density device */
